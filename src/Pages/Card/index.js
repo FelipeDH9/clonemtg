eb052e8a9e5495e3afa90fd0301734d0b049dedd
Felipe <felipehaddad25@gmail.com>
2022-05-09 21:29:25 -0300

    feat: add css into page card

--- src/Pages/Card/index.js
+++ src/Pages/Card/index.js
@@ -1,17 +1,47 @@
 import './styles.css'
 import { useParams } from 'react-router-dom'
 
-// CODE SMELLS
-// short commit
-
 function Card() {
   const { id } = useParams()
   console.log('id ----->', id)
 
   return (
     <div>
-      <h1>OUTRA PÁGINA</h1>
-      <h3>CardId = {id}</h3>
+      <div className="titles">
+        <h3 id="card-name">NOME DA CARTA</h3>
+        <h3>SET DA CARTA</h3>
+      </div>
+      <div className="card-info">
+        {/*BLOCAO COM CARTA E TEXTO*/}
+        <div className="card-image-butons">
+          <img src="https://cdn1.mtggoldfish.com/images/h/Abbot-of-Keral-Keep-ORI-672.jpg"></img>
+          <button>Add to collection</button>
+          <button>Create price Alert</button>
+        </div>
+        <div className="card-info-box">
+          <div className="info-box">
+            <h3>Abbot of Keral Keep</h3>
+            {/* <span>CUSTO DE MANA</span> */}
+            <p>Creature — Human Monk</p>
+            <p>
+              Prowess (Whenever you cast a noncreature spell, this creature gets
+              +1/+1 until end of turn.) When Abbot of Keral Keep enters the
+              battlefield, exile the top card of your library. Until end of
+              turn, you may play that card.
+            </p>
+            <div className="rarity-power">
+              <div>Rare</div>
+              <div>
+                <strong>2 / 1</strong>
+              </div>
+            </div>
+            <div className="artist">
+              #127 Illustrated by Deruchenko Alexander
+            </div>
+          </div>
+        </div>
+      </div>
+      <h3>Card Id = {id}</h3>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Pages/Home'
 import Card from './Pages/Card'
-import Layout from './components/Layout'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/global.css'
+import LayoutComponent from './components/Layout'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
-      <Layout>
+      <LayoutComponent>
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/card/:id" element={<Card />} />
           <Route path="*" element={<h1> não encontrado</h1>} />
         </Routes>
-      </Layout>
+      </LayoutComponent>
     </Router>
   </React.StrictMode>
 )
