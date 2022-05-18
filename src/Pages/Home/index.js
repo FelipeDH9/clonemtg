0b30a52db1530dfd1ccbe21cebaf7ee8d5c8990e
Felipe <felipehaddad25@gmail.com>
2022-05-18 15:36:41 -0300

    feat: add pagination on home page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,29 +1,41 @@
 import { useState, useEffect } from 'react'
 // import NoCards from '../../components/NoCards'
 import { getCards } from '../../services/magic/index'
-import { GoSearch } from 'react-icons/go'
+import { FaSearch } from 'react-icons/fa'
+import ReactPaginate from 'react-paginate'
 
 import './styles.css'
 
 function Home() {
   const [currentCards, setCurrentCards] = useState()
   const [cardName, setCardName] = useState()
+  const [currentItems, setCurrentItems] = useState([])
+  const [pageCount, setPageCount] = useState(0)
+  const [itemOffset, setItemOffset] = useState(0)
+  const itemsPerPage = 15
 
   async function fetchData() {
     const { data } = await getCards({ name: cardName })
-    console.log('chamou', data)
     if (data) {
-      const sortedCards = await data.cards.sort((a, b) =>
+      const dataArray = await data.cards
+      console.log('ARRAY ==', dataArray)
+      const sortedCards = await dataArray.sort((a, b) =>
         a.name.localeCompare(b.name)
       )
-
       setCurrentCards(sortedCards)
     }
   }
 
+  const handlePageClick = event => {
+    const newOffset = (event.selected * itemsPerPage) % currentCards.length
+    setItemOffset(newOffset)
+  }
+
   useEffect(() => {
-    fetchData()
-  }, [])
+    const endOffset = itemOffset + itemsPerPage
+    setCurrentItems(currentCards?.slice(itemOffset, endOffset))
+    setPageCount(Math.ceil(currentCards?.length / itemsPerPage))
+  }, [itemOffset, itemsPerPage, currentCards])
 
   return (
     <div className="body-home">
@@ -37,35 +49,76 @@ function Home() {
         ></input>
 
         <button className="button" onClick={() => fetchData()}>
-          <GoSearch />
+          <FaSearch />
         </button>
       </div>
 
       <table id="cardList">
         <tbody>
-          {currentCards?.map(card => {
+          {currentItems?.map(card => {
             return (
-              <>
+              <tr key={card.id}>
+                <td className="card-name">
+                  {card.imageUrl ? (
+                    <a href={`/card/${card.id}`} className="tooltip">
+                      {card.name}
+                      <img
+                        className="tooltipimage"
+                        src={card.imageUrl}
+                        alt="card"
+                      ></img>
+                    </a>
+                  ) : (
+                    <p>{card.name}</p>
+                  )}
+                </td>
+                <td>{card.setName}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {/* <table id="cardList">
+        <tbody>
+          {currentItems?.map(card => {
+            return (
+              <tr>
                 {card.imageUrl && (
-                  <tr key={card.id}>
+                  <tr>
                     <td>
                       <a href={`/card/${card.id}`} className="tooltip">
                         {card.name}
                         <img
                           className="tooltipimage"
                           src={card.imageUrl}
-                          alt="card"
+                          alt={card.name}
                         ></img>
                       </a>
                     </td>
                     <td>{card.setName}</td>
                   </tr>
                 )}
-              </>
+              </tr>
             )
           })}
         </tbody>
-      </table>
+      </table> */}
+      {currentItems && (
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          pageLinkClassName="page-num"
+          previousLinkClassName="page-num"
+          nextLinkClassName="page-num"
+          activeLinkClassName="active"
+        />
+      )}
     </div>
   )
 }
