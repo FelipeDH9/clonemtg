71119e76d3ad17a8a1e0cac83c6eb9a37c38fbf6
Felipe <felipehaddad25@gmail.com>
2022-06-22 12:29:40 -0300

    fix: change life register to table and style changes

--- src/Pages/LifeCount/index.js
+++ src/Pages/LifeCount/index.js
@@ -1,82 +1,93 @@
-import { calculateNewValue } from '@testing-library/user-event/dist/utils'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './styles.css'
 
 function LifeCount() {
   const [myLife, setMyLife] = useState(20)
   const [myRegister, setMyRegister] = useState([20])
-  const [myLifeChange, setMyLifeChange] = useState([])
 
   const [hisLife, setHisLife] = useState(20)
   const [hisRegister, setHisRegister] = useState([20])
-  const [hisLifeChange, setHisLifeChange] = useState([])
 
   function changeMyLife(value) {
     const myNewLife = parseInt(myLife) + parseInt(value)
+    const myOldLife = myLife
 
-    setMyRegister(e => [...e, { myNewLife }])
-    console.log('myRegister', myRegister)
+    setMyRegister(e => [...e, { myOldLife, value, myNewLife }])
 
     setMyLife(eval(myNewLife))
-    setMyLifeChange(n => [...n, { value }])
-    console.log('myLifeChange', myLifeChange)
   }
 
   function changeHisLife(value) {
     const hisNewLife = parseInt(hisLife) + parseInt(value)
+    const hisOldLife = hisLife
 
-    setHisRegister(e => [...e, { hisNewLife }])
-    console.log('hisRegister', hisRegister)
+    setHisRegister(e => [...e, { hisOldLife, value, hisNewLife }])
 
     setHisLife(eval(hisNewLife))
-    setHisLifeChange(n => [...n, { value }])
-    console.log('hisLifeChange', hisLifeChange)
   }
-
-  useEffect(() => {}, [])
 
   return (
     <div className="container2">
       <div className="lifeBlock">
         <div className="lifeRegister">
-          <div>
-            {myLifeChange?.map(e => (
-              <p>{e.value}</p>
-            ))}
-          </div>
-          <div>
-            {myRegister?.map(e => (
-              <p>{e.myNewLife}</p>
-            ))}
-          </div>
+          <table className="lifeTable">
+            <tbody>
+              {myRegister?.map(e => (
+                <tr>
+                  <td>
+                    {e.value >= 0 ? (
+                      <p style={{ color: '#8af34d' }}>+{e.value}</p>
+                    ) : (
+                      <p style={{ color: '#eb5555' }}>{e.value}</p>
+                    )}
+                  </td>
+                  <td>{e.myNewLife}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
         <div className="lifeAndButtons">
           <p className="life">{myLife}</p>
           <div className="buttons-wrapper">
             <button
-              className="btn+1"
+              className="btnPlus1 btnPlus"
               value={+1}
               onClick={event => changeMyLife(event.target.value)}
             >
               +1
             </button>
             <button
-              className="btn-1"
+              className="btnMinus1 btnMinus"
               value={-1}
               onClick={event => changeMyLife(event.target.value)}
             >
               -1
             </button>
             <button
+              className="btnPlus2 btnPlus"
+              value={+2}
+              onClick={event => changeMyLife(event.target.value)}
+            >
+              +2
+            </button>
+            <button
+              className="btnMinus2 btnMinus"
+              value={-2}
+              onClick={event => changeMyLife(event.target.value)}
+            >
+              -2
+            </button>
+            <button
               value={+3}
-              className="btn+3"
+              className="btnPlus3 btnPlus"
               onClick={event => changeMyLife(event.target.value)}
             >
               +3
             </button>
             <button
               value={-3}
-              className="btn-3"
+              className="btnMinus3 btnMinus"
               onClick={event => changeMyLife(event.target.value)}
             >
               -3
@@ -87,44 +98,64 @@ function LifeCount() {
 
       <div className="lifeBlock">
         <div className="lifeRegister">
-          <div>
-            {hisLifeChange?.map(e => (
-              <p>{e.value}</p>
-            ))}
-          </div>
-          <div>
-            {hisRegister?.map(e => (
-              <p>{e.hisNewLife}</p>
-            ))}
-          </div>
+          <table className="lifeTable">
+            <tbody>
+              {hisRegister?.map(e => (
+                <tr>
+                  <td>
+                    {e.value >= 0 ? (
+                      <p style={{ color: '#8af34d' }}>+{e.value}</p>
+                    ) : (
+                      <p style={{ color: '#eb5555' }}>{e.value}</p>
+                    )}
+                  </td>
+                  <td>{e.hisNewLife}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
         <div className="lifeAndButtons">
           <p className="life">{hisLife}</p>
           <div className="buttons-wrapper">
             <button
-              className="btn+1"
+              className="btnPlus1 btnPlus"
               value={+1}
               onClick={event => changeHisLife(event.target.value)}
             >
               +1
             </button>
             <button
-              className="btn-1"
+              className="btnMinus1 btnMinus"
               value={-1}
               onClick={event => changeHisLife(event.target.value)}
             >
               -1
             </button>
             <button
+              className="btnPlus2 btnPlus"
+              value={+2}
+              onClick={event => changeHisLife(event.target.value)}
+            >
+              +2
+            </button>
+            <button
+              className="btnMinus2 btnMinus"
+              value={-2}
+              onClick={event => changeHisLife(event.target.value)}
+            >
+              -2
+            </button>
+            <button
               value={+3}
-              className="btn+3"
+              className="btnPlus3 btnPlus"
               onClick={event => changeHisLife(event.target.value)}
             >
               +3
             </button>
             <button
               value={-3}
-              className="btn-3"
+              className="btnMinus3 btnMinus"
               onClick={event => changeHisLife(event.target.value)}
             >
               -3
